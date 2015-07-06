f0245b432e7115d5c1adabee5d37edf08489f062
douglas.beniz <douglas.beniz@8c29a70a-4b4f-4fad-ae86-83d8b3214d8e>
2015-07-06 23:05:37 +0000

    [douglas.beniz] Implemented all Get methods;
    git-svn-id: https://svn.lnls.br/LNLS/Py4Syn/trunk@257 8c29a70a-4b4f-4fad-ae86-83d8b3214d8e

--- py4syn/epics/LakeShore331Class.py
+++ py4syn/epics/LakeShore331Class.py
@@ -49,7 +49,7 @@ class LakeShore331 (IScannable, StandardDevice):
             LakeShore331's mnemonic
         """
         StandardDevice.__init__(self, mnemonic)
-        self.keithley = Device(pvName+':', 
+        self.lakeshore331 = Device(pvName+':', 
                                ('GetAHEAT', 'GetAHeaterRange', 'GetAPIDD', 'GetAPIDI', 'GetAPIDP',
                                 'GetASetPoint', 'GetBHEAT', 'GetBHeaterRange', 'GetBPIDD', 'GetBPIDI',
                                 'GetBPIDP', 'GetBSetPoint', 'GetCTempA', 'GetCTempB', 'GetKTempA', 
@@ -59,6 +59,263 @@ class LakeShore331 (IScannable, StandardDevice):
         self.control_setAPID = PV(pvName + ':CONTROL:SetAPID')
         self.control_setBPID = PV(pvName + ':CONTROL:SetBPID')
         self.control_trigger = PV(pvName + ':CONTROL:Trigger')
+
+    def getAHeat(self):
+        """
+        Heater output query for A channel.
+
+        Returns
+        -------
+        Value: Float, e.g.: 0.001
+
+        Examples
+        --------
+        >>> ls331.getAHeat()
+        >>> 51.530
+        """
+
+        return self.lakeshore331.get('GetAHEAT')
+
+    def getBHeat(self):
+        """
+        Heater output query for B channel.
+
+        Returns
+        -------
+        Value: Float, e.g.: 0.001
+
+        Examples
+        --------
+        >>> ls331.getBHeat()
+        >>> 51.530
+        """
+
+        return self.lakeshore331.get('GetBHEAT')
+
+    def getAHeaterRange(self):
+        """
+        Heater range command for A channel.
+
+        Returns
+        -------
+        Value: Float, e.g.: 0.001
+
+        Examples
+        --------
+        >>> ls331.getAHeaterRange()
+        >>> 51.530
+        """
+
+        return self.lakeshore331.get('GetAHeaterRange')
+
+    def getBHeaterRange(self):
+        """
+        Heater range command for B channel.
+
+        Returns
+        -------
+        Value: Float, e.g.: 0.001
+
+        Examples
+        --------
+        >>> ls331.getBHeaterRange()
+        >>> 51.530
+        """
+
+        return self.lakeshore331.get('GetBHeaterRange')
+
+    def getAPIDD(self):
+        """
+        Returns Value D of PID for channel A.
+
+        Returns
+        -------
+        Value: Integer, e.g.: 10
+
+        Examples
+        --------
+        >>> ls331.getAPIDD()
+        >>> 31
+        """
+
+        return self.lakeshore331.get('GetAPIDD')
+
+    def getBPIDD(self):
+        """
+        Returns Value D of PID for channel B.
+
+        Returns
+        -------
+        Value: Integer, e.g.: 10
+
+        Examples
+        --------
+        >>> ls331.getBPIDD()
+        >>> 32
+        """
+
+        return self.lakeshore331.get('GetBPIDD')
+    
+    def getAPIDI(self):
+        """
+        Returns Value I of PID for channel A.
+
+        Returns
+        -------
+        Value: Integer, e.g.: 10
+
+        Examples
+        --------
+        >>> ls331.getAPIDI()
+        >>> 31
+        """
+
+        return self.lakeshore331.get('GetAPIDI')
+
+    def getBPIDI(self):
+        """
+        Returns Value I of PID for channel B.
+
+        Returns
+        -------
+        Value: Integer, e.g.: 10
+
+        Examples
+        --------
+        >>> ls331.getBPIDI()
+        >>> 32
+        """
+
+        return self.lakeshore331.get('GetBPIDI')
+
+    def getAPIDP(self):
+        """
+        Returns Value P of PID for channel A.
+
+        Returns
+        -------
+        Value: Integer, e.g.: 10
+
+        Examples
+        --------
+        >>> ls331.getAPIDP()
+        >>> 31
+        """
+
+        return self.lakeshore331.get('GetAPIDP')
+
+    def getBPIDP(self):
+        """
+        Returns Value P of PID for channel B.
+
+        Returns
+        -------
+        Value: Integer, e.g.: 10
+
+        Examples
+        --------
+        >>> ls331.getBPIDP()
+        >>> 32
+        """
+
+        return self.lakeshore331.get('GetBPIDP')
+
+
+    def getASetPoint(self):
+        """
+        Returns setpoint value for channel A.
+
+        Returns
+        -------
+        Value: float, e.g.: 0.001 
+        
+        Examples
+        --------
+        >>> ls331.getASetPoint()
+        >>> 67.87
+        """
+
+        return self.lakeshore331.get('GetASetPoint')
+
+    def getBSetPoint(self):
+        """
+        Returns setpoint value for channel B.
+
+        Returns
+        -------
+        Value: float, e.g.: 0.001 
+        
+        Examples
+        --------
+        >>> ls331.getBSetPoint()
+        >>> 67.87
+        """
+
+        return self.lakeshore331.get('GetBSetPoint')
+
+    def getCTempA(self):
+        """
+        Returns channel A temperature in Celsius degrees.
+
+        Returns
+        -------
+        Value: float, e.g.: 0.001
+
+        Examples
+        --------
+        >>> ls331.getCTempA()
+        >>> 32.56
+        """
+
+        return self.lakeshore331.get('GetCTempA')
+
+    def getCTempB(self):
+        """
+        Returns channel B temperature in Celsius degrees.
+
+        Returns
+        -------
+        Value: float, e.g.: 0.001
+
+        Examples
+        --------
+        >>> ls331.getCTempB()
+        >>> 32.56
+        """
+
+        return self.lakeshore331.get('GetCTempB')
+
+    def getKTempA(self):
+        """
+        Returns channel A temperature in Kelvin.
+
+        Returns
+        -------
+        Value: float, e.g.: 0.001
+
+        Examples
+        --------
+        >>> ls331.getKTempA()
+        >>> 32.56
+        """
+
+        return self.lakeshore331.get('GetKTempA')
+
+    def getKTempB(self):
+        """
+        Returns channel B temperature in Kelvin.
+
+        Returns
+        -------
+        Value: float, e.g.: 0.001
+
+        Examples
+        --------
+        >>> ls331.getKTempB()
+        >>> 32.56
+        """
+
+        return self.lakeshore331.get('GetKTempB')
 
     def getValue(self):
         """
