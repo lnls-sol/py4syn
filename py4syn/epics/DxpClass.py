19bd6f378ce11fd660c21cf1153f7989294397be
Gabriel Fedel <fedel@fedel.net.br>
2016-11-10 18:23:40 -0200

    Unified my version with Juliano version

--- py4syn/epics/DxpClass.py
+++ py4syn/epics/DxpClass.py
@@ -6,49 +6,73 @@ Python Class for EPICS Dxp Control.
 :synopsis: Python Class for EPICS Spectro control.
 
 .. moduleauthor:: Gabriel Fedel <gabriel.fedel@lnls.br>
-    .. note:: /06/2016 [gabrielfedel]  first version released
+.. based on dxpclass from Juliano Murari and Pilatus Class from Henrique Almeida
+    .. note:: /10/2016 [gabrielfedel]  first version released
 """
 from epics import PV, ca
 from py4syn.epics.StandardDevice import StandardDevice
 from py4syn.epics.ICountable import ICountable
 import numpy as np
+from threading import Eventa
+from py4syn.utils.timer import Timer
 
 
 class Dxp(StandardDevice, ICountable):
 
-    def onValChange(self, value, **kw):
-        self._counting = value
 
     # CONSTRUCTOR OF DXP CLASS
-    def __init__(self, pvDxpName="", dxpType="mca", channels=4, mnemonic="",numberOfRois=32 ):
-        StandardDevice.__init__(self, mnemonic)
-        self._counting = False
+    def __init__(self, mnemonic, numberOfChannels=4, numberOfRois=32, pv, dxpType="mca", responseTimeout = 15 ):
+        """ Constructor
+        responseTimeout : how much time to wait dxp answer
+        """
+        super().__init__(mnemonic)
+        self.acquireChanged = Event()
+        self.acquiring = False
 
+        # TODO: removes after teste
         # determines the exposition time (live time)
-        self.pvDxpTime = PV(pvDxpName+":PresetLive.VAL")
+        #self.pvDxpTime = PV(pv+":PresetLive.VAL")
         # determines the start of counting
-        self.pvDxpEraseStart = PV(pvDxpName+":EraseStart.VAL")
+        self.pvDxpEraseStart = PV(pv+":EraseStart.VAL")
         # determines mode of counting (Live Time, Real Time, ...)
-        self.pvDxpPresetMode = PV(pvDxpName+":PresetMode.VAL")
-        # TODO:maybe this is not necessary
-        self.pvDxpStop = PV(pvDxpName+":StopAll.VAL")
+        self.pvDxpPresetMode = PV(pv+":PresetMode.VAL")
+
+        self.pvDxpStop = PV(pv+":StopAll.VAL")
         # store all channels
         self.pvDxpChannels = []
         # store ROIs
         self.pvDxpRois = []
 
-        for c in range(1, channels+1):
-            self.pvDxpChannels.append(PV(pvDxpName+":"+dxpType+str(c)))
+        # store Acquire Time for each channel
+        self.pvDxpAcquireTime = []
+
+
+        for c in range(0, numberOfChannels):
+            # store each channel
+            self.pvDxpChannels.append(PV(pv+":"+dxpType+str(c+1)))
+            # for each channel store the PV for AcquireTime
+            self.pvDxpAcquireTime.append(PV(pv+":" + dxpType + "%d.PLTM" % (c+1)))
             self.pvDxpRois.append([])
             # storeing each ROI in your channel
             for r in range(0,numberOfRois):
-                self.pvDxpRois[c-1].append(PV(pvDxpName+":"+dxpType+str(c)+'.R'+str(r)))
+                self.pvDxpRois[c].append(PV(pv+":"+dxpType+str(c+1)+'.R'+str(r)))
 
-        # TODO: verify if onValchange is working correctly
-        self.pvDxpAcquiring = PV(pvDxpName+":Acquiring", self.onValChange)
-        self.channels = channels
+        self.pvDxpAcquiring = PV(pv+":Acquiring", self.onValChange)
+        self.pvDxpAcquire.add_callback(self.statusChange)
+        self.channels = numberOfChannels
         self.dxpType = dxpType
+        self.rois = numberOfRois
 
+        self.responseTimeout = responseTimeout
+        self.timer = Timer(self.responseTimeout)
+
+    def statusChange(self, value, **kw):
+        """
+        Helper callback used to wait for the end of the acquisition.
+        """
+        self.acquiring = value
+        # threads waiting are awakened
+        self.acquireChanged.set()
 
     def setCountTime(self, time):
         """
@@ -56,26 +80,22 @@ class Dxp(StandardDevice, ICountable):
 
         Parameters
         ----------
-        t : value
+        time : `float`
             Count time to set to scaler device .
 
         Returns
         -------
         out : None
         """
-        self.pvDxpTime.put(time)
-
+        for i in range(0,self.numberOfChannels):
+            self.pvAcquireTime[i].put(time, wait=True)
+        self.timer = Timer(time + self.responseTime)
 
     def getCountTime(self):
         return self.pvDxpTime.get()
 
-    def setCountStart(self):
-        self._counting = True
-        # TODO: testar para verificar qual melhor maneira, utilizando EraseStart ou caget
-        self.pvDxpEraseStart.put(1)
-
     def setCountStop(self):
-        self.pvDxpStop.put(1)
+        self.pvDxpStop.put(1, wait = True)
 
     def getValueChannel(self, channel, asnumpy = True, **kwargs):
         """Return intensity
@@ -86,45 +106,78 @@ class Dxp(StandardDevice, ICountable):
            r = int(channel[5])
            return self.pvDxpRois[c][r]
         else:
+            # TODO on this way returns the points. Find a better way
+            # That work for many points, probably remove asnumpy
             return self.pvDxpChannels[c].get(as_numpy = asnumpy)
 
-    def getIntensityInTime(self, time, channel=2):
-        self.setCountTime(time)
-        self.setCountStart()
-        self.wait()
-        return self.getIntensity(channel)
+# TODO: remove after confirm that is not necessary
+#    def getIntensityInTime(self, time, channel=2):
+#        self.setCountTime(time)
+#        self.setCountStart()
+#        self.wait()
+#        return self.getIntensity(channel)
 
     def isCountRunning(self):
         return (self.pvDxpAcquiring.get())
 
     def wait(self):
-        while(self._counting):
-            ca.poll()
+        """
+        Blocks until the acquisition completes.
+        """
+        if self.acquiring == False:
+            return
+
+        self.acquireChanged.clear()
+        # while acquiring and not time out waits
+        # TODO: find a better way to do this
+        while self.acquiring and self.timer.check():
+            self.acquireChanged.wait(0.001)
+            self.acquireChanged.clear()
+
+        if self.timer.expired():
+            raise RuntimeError('DXP is not answering')
+
 
     def canMonitor(self):
-        return True
+        """ Returns false indcating Dxp cannot be use as a counter monitor"""
+        return False
 
     def canStopCount(self):
+         """
+        Returns true indicating that Dxp has a stop command.
+        """
         return True
 
     def getValue(self, **kwargs):
         """
         This is a dummy method that always returns zero, which is part of the
-        :class:`py4syn.epics.ICountable` interface. Dxo does not return
-        a value while scanning. Instead, it stores a mca file with reult .
+        :class:`py4syn.epics.ICountable` interface. Dxp does not return
+        a value while scanning. Instead, it stores a mca file with result .
         """
-        return 0
+        if(kwargs):
+            return self.getValueChannel(kwargs['channel'], kwargs['asnumpy'])
+        return self.getValueChannel()
 
     def isCounting(self):
-        return self._counting
+        return self.acquiring
 
     def startCount(self):
-        self.setCountStart()
+        """ Starts acquiring an spectrum
+        It's necessary to call setCounTime before"""
+
+        if self.acquiring:
+            raise RuntimeError('Already counting')
+
+        self.acquiring = True
+        self.pvEraseStart.put(1)
+        # resets initial time value
+        self.timer.mark()
 
     def stopCount(self):
         self.setCountStop()
 
     def setPresetValue(self, channel, val):
+        """Dummy method"""
         pass
 
     def close(self):
