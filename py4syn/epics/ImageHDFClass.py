95e15681a5ce9d46622524ca863dbb259d22841d
Gabriel Fedel <fedel@fedel.net.br>
2017-02-07 10:30:40 -0200

    Finish first version of ImageHDFClass

--- py4syn/epics/ImageHDFClass.py
+++ py4syn/epics/ImageHDFClass.py
@@ -8,92 +8,85 @@ Python Class for Save HDF images .
 .. moduleauthor:: Gabriel Fedel <gabriel.fedel@lnls.br>
 .. note:: 06/02/2017 [gabrielfedel]  first version released
 """
-from epics import PV
-from py4syn.epics.StandardDevice import StandardDevice
-from py4syn.epics.ICountable import ICountable
 import numpy as np
-from threading import Event
-from py4syn.utils.timer import Timer
 import os
 import h5py
 
 
 class ImageHDF():
     # CONSTRUCTOR OF ImageHDF CLASS
-    def __init__(self,  numPoints):
+    def __init__(self,  numPoints, output, prefix, sufix):
         """ Constructor
+        prefix: prefix for filenames
+        sufix: sufix for filenames
         """
         self.numPoints = numPoints
         self.image = None
+        self.lastPos = -1
+        self.output = output
+        self.prefix = prefix
+        self.sufix = sufix
 
 
-    def nameFile(self):
-        '''Generate correct name to file'''
-        pass
-    # save the spectrum intensity in a mca file if is a point
-    # or an hdf file if is an image
-    def saveSpectrum(self, spectrum):
+    def nameFile(self, output, prefix, sufix):
+        '''Generate correct name to file
+        output: original fileName
+        prefix: added after id
+        sufix: extension'''
+
+        start = fileName.split('.')[0]
+
+        idx = 0
+        while os.path.exists('%s%s_%04d.%s' % (start, prefix, idx, sufix)):
+            idx += 1
+
+        resultName= '%s%s_%04d.%s' % (start, prefix, idx, sufix)
+
+        return resultName
+
+    def saveSpectrum(self, spectrum, snake = True):
+        ''' save the spectrum intensity in a mca file if is a point
+            or an hdf file if is an image
+            snake: if data is collected on snake mode'''
+        self.spectrum = spectrum
         # save a unique point
         if self.image is None:
-            fileName = self.fileName
-            idx = 1
-            if(fileName):
-                prefix = fileName.split('.')[0]
-                while os.path.exists('%s_%s%d_%04d.mca' % (prefix,
-                                                           self.dxpType,
-                                                           self.ch, idx)):
-                    idx += 1
-                fileName = '%s_%s%d_%04d.mca' % \
-                           (prefix, self.dxpType, self.ch, idx)
-                np.savetxt(fileName, self.spectrum, fmt='%f')
+            fileName = self.nameFile(self.output, self.prefix, self.sufix)
+            np.savetxt(fileActual, self.spectrum, fmt='%f')
         else:
             # add a point on hdf file
-            self.spectrum[0] = self.lastPos
             self.col = int(self.lastPos/self.rows)
             self.row = self.lastPos - self.rows*self.col
-            # if is an odd line
-            if (self.col % 2 != 0):
-                self.row = -1*(self.row+1)
+            if snake:
+                # if is an odd line
+                if (self.col % 2 != 0):
+                    self.row = -1*(self.row+1)
             self.image[self.col, self.row, :] = self.spectrum
 
             self.lastPos += 1
 
-    def startCollectImage(self, rows=0, cols=0):
+    def startCollectImage(self, rows=0, cols=0, dtype):
         """Start to collect an image
         When collect an image, the points will be  saved on a hdf file"""
         self.rows = rows
         self.cols = cols
         # create HDF file
-        fileName = self.fileName
-        prefix = fileName.split('.')[0]
-
-        # TODO: include channel on fileName
-
-        fileName = self.fileName
-        idx = 1
-
-        while os.path.exists('%s_%s_%04d.hdf' % (prefix, self.dxpType, idx)):
-            idx += 1
-        fileName = '%s_%s_%04d.hdf' % (prefix, self.dxpType, idx)
+        fileName = self.nameFile(self.output, self.prefix, self.sufix)
 
         self.fileResult = h5py.File(fileName)
 
         # TODO: review this
-        lineShape = (1, self.rows, self.imageDeep)
-        # TODO: verify if it's better create it with complete or
-        # resize on each point
-        # TODO: verify if dtype is always int32
-        # create "image"
+        lineShape = (1, self.rows, self.numPoints)
         self.image = self.fileResult.create_dataset(
                      'data',
-                     shape=(self.cols, self.rows, self.imageDeep),
-                     dtype='int32',
+                     shape=(self.cols, self.rows, self.numPoints),
+                     dtype=dtype,
                      chunks=lineShape)
 
         # create "image" normalized
         self.imageNorm = self.fileResult.create_dataset(
                      'data_norm',
-                     shape=(self.cols, self.rows, self.imageDeep),
+                     shape=(self.cols, self.rows, self.numPoints),
                      dtype='float32',
                      chunks=lineShape)
 
@@ -110,17 +103,8 @@ class ImageHDF():
         result = np.divide(self.spectrum, float(value))
         if self.image is None:
             # normalization for a point
-            fileName = self.fileName
-            idx = 1
-            if(fileName):
-                prefix = fileName.split('.')[0]
-                while os.path.exists('%s_%s%d_%04d_norm.mca' % (prefix,
-                                                                self.dxpType,
-                                                                self.ch, idx)):
-                    idx += 1
-                fileName = '%s_%s%d_%04d_norm.mca' % \
-                           (prefix, self.dxpType, self.ch, idx)
-                np.savetxt(fileName, result, fmt='%f')
+            fileName = self.nameFile(self.output, self.prefix + '_norm', self.sufix)
+            np.savetxt(fileName, result, fmt='%f')
 
         else:
             self.imageNorm[self.col, self.row, :] = result
