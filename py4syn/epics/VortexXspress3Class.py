c1b2ff0aa53cf3c860e64275efab2f0e1ee0adf3
Luciano Carneiro Guedes <luciano.guedes@lnls.br>
2019-11-08 20:59:55 +0000

    Update Vortex Class

--- py4syn/epics/VortexXspress3Class.py
+++ py4syn/epics/VortexXspress3Class.py
@@ -12,7 +12,7 @@ Python class for Vortex using EPICS area detector IOC.
 
 from py4syn.epics.StandardDevice import StandardDevice
 from py4syn.epics.ICountable import ICountable
-from epics import PV
+import epics
 
 from time import sleep, time
 
@@ -23,11 +23,6 @@ class VortexXspress3(StandardDevice, ICountable):
     Examples
     --------
     """
-
-
-    RESPONSE_TIMEOUT = 15
-    WAIT_ACQUIRING = 0.005
-
 
     def onAcquireChange(self, value, **kw):
         self._done = (value == 0)
@@ -45,47 +40,14 @@ class VortexXspress3(StandardDevice, ICountable):
             Base name of the EPICS process variable
         """
         super().__init__(mnemonic)
-        self.pvAcquire = PV(pv + ':Acquire')
-        self.pvStatus = PV(pv + ':Acquire_RBV', callback=self.onAcquireChange)
-        self.pvAcquireTime = PV(pv + ':AcquireTime')
-        self.pvClear = PV(pv + ':ERASE')
+        self.pvAcquire = epics.PV(pv + ':Acquire')
+        self.pvStatus = epics.PV(pv + ':Acquire_RBV', callback=self.onAcquireChange)
+        self.pvAcquireTime = epics.PV(pv + ':AcquireTime')
+        self.pvClear = epics.PV(pv + ':ERASE')
 
         self.pvMcaCounters = []
-        self.pvStatusScan = PV(pv + ':Acquire_RBV.SCAN')
-        self.pvStatusScan.put(9)
-
-        # Channels 1-4
-        for i in range(1, 5):
-            for j in range(1, 5):            
-               self.pvMcaCounters.append(PV('{}:C{}_ROI{}:Value_RBV'.format(pv, j, i)))
-        P = pv+":HDF5:"
-        self.pvClear.put(1, wait=True)
-        self.n_extra_dim_pv =PV(P+"NumExtraDims")
-        self.x_dim_pv = PV(P+"ExtraDimSizeX")
-        self.y_dim_pv = PV(P+"ExtraDimSizeY")
-        self.write_mode_pv = PV(P+"FileWriteMode")
-        self.file_path_pv = PV(P+"FilePath")
-        self.file_name_pv = PV(P+"FileName")
-        self.start_capture_pv = PV(P+"Capture")
-        self.n_frames = PV(pv+":NumImages")
-        numb_frames = 1
-        name = 'teste'
-        #Stop writing
-        self.start_capture_pv.put(0)
-        #Set dimensions
-        self.n_extra_dim_pv.put(1)
-        #points
-        self.x_dim_pv.put(numb_frames)
-        self.y_dim_pv.put(0)
-        #Set Write Mode
-        #2=Stream
-        self.write_mode_pv.put(2)
-        #Set output path
-        self.file_path_pv.put("/data/")
-        self.file_name_pv.put(name)
-        self.n_frames.put(numb_frames)
-        self.filenumb =PV(P+"FileNumber")
-        self.filenumb.put(0)
+        self.pvStatusScan = epics.PV(pv + ':Acquire_RBV.SCAN')
+        self.pvStatusScan.put(2)
 
 	
     def close(self):
@@ -100,14 +62,7 @@ class VortexXspress3(StandardDevice, ICountable):
 
 
     def getValue(self, **kwargs):
-        if(kwargs):                 
-            count = 0            
-            value = self.getIntensity(kwargs['channel'])
-            while (value==0 and count<3):
-                sleep(.05) 
-                value = self.getIntensity(kwargs['channel'])
-                count += 1
-            return value
+        return True
 
 
     def setCountTime(self, t):
@@ -138,11 +93,9 @@ class VortexXspress3(StandardDevice, ICountable):
         """
         if not self._done:
             raise RuntimeError('Already counting')
-        self.pvClear.put(1, wait=True) # clear the ROI value before start a new acquire
-        self.start_capture_pv.put(1)
+        self.pvClear.put(1) # clear the ROI value before start a new acquire
         self.pvAcquire.put(1)
         self._done = 0 # force the confirmation that the detector has already received acquire function
-        
 
 
     def stopCount(self):
@@ -184,5 +137,4 @@ class VortexXspress3(StandardDevice, ICountable):
         Blocks until the acquisition completes.
         """
         while not self._done:
-            sleep(self.WAIT_ACQUIRING)
-        sleep(0.2)
+            epics.poll(evt=1.e-5, iot=0.1)
